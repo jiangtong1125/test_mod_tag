38c734368c014c91d72c9d21036d7416de56d243
jiangtong <jiangt@soulgame.com>
2023-04-13 14:36:19 +0800

    1.0.0.2

--- mod_tag.go
+++ mod_tag.go
@@ -9,6 +9,10 @@ func main() {
 	log.Print("version 1")
 }
 
-func abs(x float64) float64 {
+func Abs(x float64) float64 {
 	return math.Abs(x)
+}
+
+func Add(x, y float64) float64 {
+	return x + y
 }
